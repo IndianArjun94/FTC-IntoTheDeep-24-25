11c429691be3dddc0c529e33a2481dc278abba9b
IndianArjun94 <arjun459459459@gmail.com>
2024-09-15 15:30:47 -0500

    Testing out motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,10 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp
 public class Main extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
 
+    public DcMotor motor;
+
+    public void runOpMode() throws InterruptedException {
+        motor = hardwareMap.get(DcMotor.class, "motor0");
+
+        motor.setPower(0.5);
+        sleep(5000);
+        motor.setPower(0);
     }
 }
