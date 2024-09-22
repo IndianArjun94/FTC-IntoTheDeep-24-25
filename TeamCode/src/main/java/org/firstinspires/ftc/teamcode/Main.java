9963e140d31a1a85b32da4c23bbe04e7cece74ee
IndianArjun94 <arjun459459459@gmail.com>
2024-09-22 15:50:59 -0500

    Using gamepad to control motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,40 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name = "Testing Motors", group = "Learning FTC")
-public class Main extends LinearOpMode {
+public class Main extends OpMode {
 
     public DcMotorEx motor;
-
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
-
-        motor = hardwareMap.get(DcMotorEx.class, "motor0");
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        motor.setTargetPosition(50 * (int) 537.6);
-
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        print("Motor Status", "Initialized");
-
-        motor.setPower(1);
-
-        while (motor.isBusy()) {
-            print("Motor Status", "Running...");
-        }
-
-        motor.setPower(0);
-
-        print("Done", "Done");
-    }
 
     public void print(String value, String value1) {
         telemetry.addData(value, value1);
         telemetry.update();
+    }
+
+    @Override
+    public void init() {
+        //        Initialization
+        motor = hardwareMap.get(DcMotorEx.class, "motor0");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        print("Robot Status", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        motor.setPower(gamepad1.right_stick_y);
+        print("Gamepad 1: ", Float.toString(gamepad1.right_stick_y));
     }
 }
