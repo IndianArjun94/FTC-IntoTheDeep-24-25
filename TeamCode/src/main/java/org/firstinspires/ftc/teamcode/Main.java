bfb8e173c6c4341e8a813ce94c6ad938c5f845c6
Arjun Nara <152097459+IndianArjun94@users.noreply.github.com>
2024-09-29 17:59:53 -0500

    Added Servos for Arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -4,17 +4,29 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleOp Program", group = "FTC Code")
 public class Main extends OpMode {
 
+    public final float MOTOR_MULTIPLIER_PERCENTAGE_CAP = 0.8F;
+    public final float ARMROT_SPEED_CAP = 0.3F;
+
     public DcMotor leftMotor;
     public DcMotor rightMotor;
 
+    public DcMotor armMotor;
+
+    public Servo armYawServo;
+    public CRServo intakeServo;
+
     public float leftMotorSpeed = 0;
     public float rightMotorSpeed = 0;
+
+    public float armYawServoPosition = 0F;
 
     public void print(String value, String value1) {
         telemetry.addData(value, value1);
@@ -22,23 +34,28 @@ public class Main extends OpMode {
     }
 
     public void update() {
-        leftMotor.setPower(leftMotorSpeed/2);
-        rightMotor.setPower(-rightMotorSpeed/2);
+//        Robot Movement
+        leftMotor.setPower(-leftMotorSpeed*MOTOR_MULTIPLIER_PERCENTAGE_CAP);
+        rightMotor.setPower(rightMotorSpeed*MOTOR_MULTIPLIER_PERCENTAGE_CAP);
     }
 
     @Override
     public void init() {
         leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
         rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armRotationMotor");
+
         leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        TODO - reverse the motor that needs to be reversed
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        print("Robot Status", "Initialized");
+        armYawServo = hardwareMap.get(Servo.class, "armServo");
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
     }
 
     @Override
     public void loop() {
+//        Robot Movement
         if (gamepad1.left_stick_y != 0) {
             leftMotorSpeed = gamepad1.left_stick_y;
             rightMotorSpeed = gamepad1.left_stick_y;
@@ -48,10 +65,33 @@ public class Main extends OpMode {
         }
 
         if (gamepad1.right_stick_x != 0) {
-            leftMotorSpeed += gamepad1.right_stick_x;
-            rightMotorSpeed -= gamepad1.right_stick_x;
+            leftMotorSpeed -= gamepad1.right_stick_x;
+            rightMotorSpeed += gamepad1.right_stick_x;
         }
 
         update();
+
+
+//        Robot Arm
+        if (gamepad1.y) {
+            armMotor.setPower(ARMROT_SPEED_CAP);
+        } else if (gamepad1.a) {
+            armMotor.setPower(-ARMROT_SPEED_CAP);
+        } else {
+            armMotor.setPower(0);
+        }
+
+//        Arm Yaw Servo & Intake
+//        TODO Arm Yaw Servo
+        if (gamepad1.left_bumper) {
+            if (armYawServoPosition > -1) {
+                armYawServoPosition -= 1;
+            }
+        } else if (gamepad1.right_bumper) {
+            if (armYawServoPosition < 1) {
+                armYawServoPosition += 1;
+            }
+        }
+
     }
 }
