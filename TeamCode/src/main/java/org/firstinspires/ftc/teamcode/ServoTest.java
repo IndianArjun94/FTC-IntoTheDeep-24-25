8a107ab69b51a0269d1049eecdf72ff54f4bd9b6
MalavS298 <thisismalav@gmail.com>
2024-10-07 11:14:45 -0500

    Intake Servo Working Code (NEW)!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -12,14 +12,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ServoTest extends OpMode {
 
     private Servo intakeServo;
+    private Servo armServo;
 
     @Override
     public void init() {
         intakeServo=hardwareMap.get(Servo.class,"intakeServo");
+        armServo=hardwareMap.get(Servo.class,"armServo");
     }
+
 
     @Override
     public void loop() {
+
+        float triggerValue = gamepad1.left_trigger;
 
         if(gamepad1.left_bumper){
             intakeServo.setPosition(0.0);
@@ -27,9 +32,13 @@ public class ServoTest extends OpMode {
         else if(gamepad1.right_bumper){
             intakeServo.setPosition(1.0);
         }
+        else if(gamepad1.left_trigger != 0.0){
+            armServo.setPosition(triggerValue);
+        }
 
         else{
             intakeServo.setPosition(0.5);
+            armServo.setPosition(0.5);
         }
 
     }
