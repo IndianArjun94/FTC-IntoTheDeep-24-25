9ffba80e27fe591a54003765c91e00f6f564ec97
MalavS298 <thisismalav@gmail.com>
2024-10-27 14:31:44 -0500

    Auton Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Auton_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Auton_1.java
@@ -1,14 +1,13 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name ="Autonomous", group ="Learning FTC")
+@Autonomous(name ="Autonomous_Official_0.5", group ="Learning FTC")
 //@Disabled
 public class Auton_1 extends LinearOpMode {
 
@@ -31,14 +30,38 @@ public class Auton_1 extends LinearOpMode {
         rightMotor.setPower(0);
         arm.setPower(0);
 
-        rightMotor.setDirection(DcMotor.Direction.FORWARD);
-        leftMotor.setDirection(DcMotor.Direction.FORWARD);
-        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        arm.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        leftMotor.setPower(-1);
-        rightMotor.setPower(1);
-        sleep(200);
+        leftMotor.setPower(0.5);
+        rightMotor.setPower(-0.5);
+        sleep(1450);
         leftMotor.setPower(0);
         rightMotor.setPower(0);
+
+        arm.setPower(1);
+        sleep(2000);
+        arm.setPower(0);
+
+        intakeServo.setPower(-1.0);
+        sleep(500);
+        intakeServo.setPower(0);
+
+        arm.setPower(-1);
+        sleep(2000);
+        arm.setPower(0);
+
+        leftMotor.setPower(0);
+        rightMotor.setPower(1);
+        sleep(500);
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
+
+        leftMotor.setPower(0.5);
+        rightMotor.setPower(-0.5);
+        sleep(0);
+        rightMotor.setPower(0);
+
     }
 }
