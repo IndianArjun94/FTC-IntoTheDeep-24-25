a5dd895c04f14893a81a657f7afccba837cbdc59
KoolKd <arjun459459459@gmail.com>
2024-11-10 09:19:57 -0600

    Viper Slide Integration, new locking mechanism 11/10/2024

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
@@ -3,31 +3,33 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp Program", group = "Final TeleOp")
 public class TeleOp extends OpMode {
     public final float MOTOR_MULTIPLIER_PERCENTAGE_CAP = 0.8F;
     public final float ARMROT_SPEED_CAP = 0.5F;
 
+    public final int VIPER_SLIDE_MIN = 0;
+    public final int VIPER_SLIDE_MAX = 3000;
+
     public DcMotor frontLeft;
     public DcMotor frontRight;
     public DcMotor backLeft;
     public DcMotor backRight;
     public DcMotor armMotor;
+    public DcMotor viperSlide;
 
-    public Servo armYawServo;
     public CRServo intakeServo;
 
     public float frontLeftMotorSpeed = 0;
     public float frontRightMotorSpeed = 0;
     public float backLeftMotorSpeed = 0;
     public float backRightMotorSpeed = 0;
-    public float armYawServoPosition = 0F;
+    public float viperSlideSpeed = 0;
 
+//    Old Arm-Locking Mechanism
     public boolean armMotorUnlocked = false;
     public float armIdlePosition = 0;
-    public double hangServoBaseValue = 0;
 
     public void print(String value, String value1) {
         telemetry.addData(value, value1);
@@ -40,9 +42,7 @@ public class TeleOp extends OpMode {
         frontRight.setPower(frontRightMotorSpeed * MOTOR_MULTIPLIER_PERCENTAGE_CAP);
         backLeft.setPower(frontRightMotorSpeed * MOTOR_MULTIPLIER_PERCENTAGE_CAP);
         backRight.setPower(frontRightMotorSpeed * MOTOR_MULTIPLIER_PERCENTAGE_CAP);
-
-//        Servo Rotation
-        armYawServo.setPosition(armYawServoPosition/360);
+        viperSlide.setPower(viperSlideSpeed);
     }
 
     public void setMotorPower(double power) {
@@ -73,15 +73,19 @@ public class TeleOp extends OpMode {
         backLeft = hardwareMap.get(DcMotor.class, "backLeftMotor");
         backRight = hardwareMap.get(DcMotor.class, "backRightMotor");
         armMotor = hardwareMap.get(DcMotor.class, "armRotationMotor");
+        viperSlide = hardwareMap.get(DcMotor.class, "viperSlide");
+
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
 
         frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        armYawServo = hardwareMap.get(Servo.class, "armServo");
-        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+        viperSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
@@ -106,19 +110,20 @@ public class TeleOp extends OpMode {
         if (gamepad1.y || gamepad2.y) {
             armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             armMotor.setPower(ARMROT_SPEED_CAP);
-            armMotorUnlocked = true;
+//            armMotorUnlocked = true;
         } else if (gamepad1.a || gamepad2.a) {
             armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             armMotor.setPower(-ARMROT_SPEED_CAP);
-            armMotorUnlocked = true;
+//            armMotorUnlocked = true;
         } else {
-            if (armMotorUnlocked) {
-                armMotorUnlocked = false;
-                armIdlePosition = armMotor.getCurrentPosition();
-                armMotor.setTargetPosition((int)armIdlePosition);
-                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor.setPower(0.15);
-            }
+//            if (armMotorUnlocked) {
+//                armMotorUnlocked = false;
+//                armIdlePosition = armMotor.getCurrentPosition();
+//                armMotor.setTargetPosition((int)armIdlePosition);
+//                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//                armMotor.setPower(0.15);
+//            }
+            armMotor.setPower(0);
         }
 
 //        Arm Lock Servo - DISABLED
@@ -128,21 +133,32 @@ public class TeleOp extends OpMode {
 //            hangServo.setPosition(hangServoBaseValue);
 //        }
 
-//        Arm Yaw Servo
-        if (gamepad2.left_bumper) { // Arm Yaw Servo
-            if (armYawServoPosition < 300) {
-                armYawServoPosition += 1;
-            }
-        } else if (gamepad2.right_bumper) { // Intake Servo
-            if (armYawServoPosition > -300) {
-                armYawServoPosition += -1;
-            }
+//        Arm Yaw Servo - DISABLED
+//        if (gamepad2.left_bumper) { // Arm Yaw Servo
+//            if (armYawServoPosition < 300) {
+//                armYawServoPosition += 1;
+//            }
+//        } else if (gamepad2.right_bumper) { // Intake Servo
+//            if (armYawServoPosition > -300) {
+//                armYawServoPosition += -1;
+//            }
+//        }
+
+//        Viper Slide
+        if (gamepad1.right_trigger != 0) {
+            viperSlide.setTargetPosition(VIPER_SLIDE_MAX);
+            viperSlide.setPower(gamepad1.right_trigger);
+        } else if (gamepad1.left_trigger != 0) {
+            viperSlide.setTargetPosition(VIPER_SLIDE_MIN);
+            viperSlide.setPower(-gamepad1.left_trigger);
+        } else {
+            viperSlide.setPower(0);
         }
 
 //        Intake Servo
-        if (gamepad2.right_trigger != 0) {
+        if (gamepad2.right_bumper) {
             intakeServo.setPower(1);
-        } else if (gamepad2.left_trigger != 0) {
+        } else if (gamepad2.left_bumper) {
             intakeServo.setPower(-1);
         } else if (gamepad2.x) {
             intakeServo.setPower(0);
