d7e17f2da02aff33d9841f9996465d8a5c1f07a7
KoolKd <arjun459459459@gmail.com>
2025-01-06 13:16:44 -0600

    1/6/2025 - Started autons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Auton_Sample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Auton_Sample.java
@@ -3,11 +3,17 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Commons;
+
 @Autonomous(name = "Auton Samples", group = "Auton FINAL")
 public class Auton_Sample extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        Commons.init(hardwareMap, this::opModeIsActive, telemetry);
+        Commons.moveForward(6, 0.5);
+        Commons.PID_rotateLeft(90, 0.7);
+        Commons.PID_forward(42, 0.8);
+        Commons.PID_rotateLeft(45, 0.5);
     }
 }
